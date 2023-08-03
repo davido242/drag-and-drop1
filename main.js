0804cc0f890ef5d2120157b57140e7d1d2db83d0
Monday_David_Sarka <david_md@hamari.agency>
2023-08-03 17:04:39 +0100

    Added script for full interaction and functionality

--- main.js
+++ main.js
@@ -1,8 +1,23 @@
-let n = 0;
+// Dom elements
+const boxes = document.querySelectorAll(".box");
+images = document.querySelector(".image");
 
-while (n < 3) {
-  n++;
-  console.log(n, "Dave");
-}
+// Loop through each boxes element
+boxes.forEach((box) => {
+  // when a draggable element drags over a box element
+  box.addEventListener("dragover", (e) => {
+    e.preventDefault(); // Prevents default behavior
+    box.classList.add("hovered")
+  });
 
-// Expected output: 3
+  // when a draggable element leaves a box element
+  box.addEventListener("dragleave", () => {
+    box.classList.remove("hovered")
+  });
+
+  // when a draggable element is dropped a box element
+  box.addEventListener("drop", () => {
+    box.append(images);
+    box.classList.remove("hovered")
+  })
+});
