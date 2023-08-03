fa026dbfa72c6a2c63fd379cfad3fd447c6cbc75
Monday_David_Sarka <david_md@hamari.agency>
2023-08-03 20:35:56 +0100

    Some cleanup

--- main.js
+++ main.js
@@ -1,23 +1,23 @@
 // Dom elements
 const boxes = document.querySelectorAll(".box");
-images = document.querySelector(".image");
+const images = document.querySelector(".image");
 
 // Loop through each boxes element
 boxes.forEach((box) => {
   // when a draggable element drags over a box element
   box.addEventListener("dragover", (e) => {
-    e.preventDefault(); // Prevents default behavior
-    box.classList.add("hovered")
+    e.preventDefault();
+    box.classList.add("hovered");
   });
 
   // when a draggable element leaves a box element
   box.addEventListener("dragleave", () => {
-    box.classList.remove("hovered")
+    box.classList.remove("hovered");
   });
 
   // when a draggable element is dropped a box element
   box.addEventListener("drop", () => {
     box.append(images);
-    box.classList.remove("hovered")
-  })
+    box.classList.remove("hovered");
+  });
 });
